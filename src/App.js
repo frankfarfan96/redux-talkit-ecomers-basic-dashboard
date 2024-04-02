9a7a88e5cec3e6b8d7d78308b04fe277c167a229
frank <frankfarfan96@gmail.com>
2024-04-02 10:31:14 +0200

    add store redux component and add Provider to RootLayout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom';
-import Product from './components/Product';
 import Dashboard from './components/Dashboard';
 import Cart from './components/Cart';
 import RootLayout from './components/RootLayout';
--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,13 +1,18 @@
 import {Outlet} from "react-router-dom";
 import NavbarPanel from "./NavbarPanel";
+import { Provider } from "react-redux";
+import store from "../store/store";
 
 const RootLayout = () => {
     return(
         <>
+        <Provider store={store}>
+
             <NavbarPanel />
             <main>
                 <Outlet />
             </main>
+        </Provider>
         </>
     )
 }
