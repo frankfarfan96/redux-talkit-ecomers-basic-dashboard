2cf7f0e6fef5c27a56faa27af2bf11f2665c0137
frank <frankfarfan96@gmail.com>
2024-04-02 10:07:23 +0200

    add reducer add for cartSlice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -5,4 +5,12 @@ const initialState = [];
 const cartSlice = createSlice({
     name : 'cart',
     initialState,
+    reducers: {
+        add(state, action) {
+            state.push(action.payload)
+        }
+    }
 });
+
+export const {add} = cartSlice.actions;
+export default cartSlice.reducer;
