bfd7722b64c0486ab51f6458f29229ef7c7a63c3
frank <frankfarfan96@gmail.com>
2024-04-02 11:56:33 +0200

    added cards to component Cart using useSelector

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,36 @@
+import { useSelector } from "react-redux";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 const Cart = () => {
+
+    const products = useSelector(state => state.cart);
+
+    const cards = products.map(product => (
+        <div className="col-md-1"  style={{ margin: '5px'}}>
+        <Card key={product.id} className="h-100">
+            <div className="text-center">
+                <Card.Img variant="top" src={product.image} style={{width:'100px', height: '130px'}} />
+            </div>
+            <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>
+                 EU : {product.price}
+                </Card.Text>
+            </Card.Body>
+            
+            <Card.Footer style={{background: 'white'}}>
+                <Button variant="danger">Remove Item</Button>
+            </Card.Footer>
+        </Card>   
+    </div>
+    ))
+
     return(
         <>
-            <h2>Cart</h2>
+            <div>
+                {cards}
+            </div>
         </>
     )
 }
